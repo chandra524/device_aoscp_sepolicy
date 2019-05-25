c4c64e96a2881e2a9e66df3e7927b958243dab22
Chris Crump <chris@cypheros.co>
2019-05-25 05:20:39 -0400

    sepolicy: Update platform paths for aoscp
    Change-Id: I9d785815916323c3104522c6144ceaaa4d35336c

--- qcom/sepolicy.mk
+++ qcom/sepolicy.mk
@@ -1,11 +1,11 @@
 #
 # This policy configuration will be used by all qcom products
-# that inherit from Lineage
+# that inherit from CypherOS
 #
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/lineage/sepolicy/qcom/private
+    device/aoscp/sepolicy/qcom/private
 
 BOARD_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/qcom/common \
-    device/lineage/sepolicy/qcom/$(TARGET_BOARD_PLATFORM)
+    device/aoscp/sepolicy/qcom/common \
+    device/aoscp/sepolicy/qcom/$(TARGET_BOARD_PLATFORM)
