361db9487f745d3ac6ae3de160c57819efed3d38
Chris Crump <chris@cypheros.co>
2019-05-25 06:16:22 -0400

    sepolicy: Guard under Luna platform
    Change-Id: Ib6480cf70f7f75c246510e8e30087a908f66d662

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -3,6 +3,7 @@
 # inherit from CypherOS
 #
 
+ifeq ($(TARGET_LUNA_PLATFORM_SEPOLICY),true)
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
     device/aoscp/sepolicy/common/public
 
@@ -11,3 +12,4 @@ BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
 
 BOARD_SEPOLICY_DIRS += \
     device/aoscp/sepolicy/common/vendor
+endif
